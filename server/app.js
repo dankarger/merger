28b66cfc614f593b2bd82505260870b89a188ef3
dankarger <karger.dan@gmail.com>
2022-02-01 13:04:54 +0200

    add app.js in server add React app

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const  app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static());
+
+
+app.use('*', (req,res)=> {
+    res.send("this routes does not exist")
+});
+
+const PORT = process.env.PORT || 8080 ;
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
