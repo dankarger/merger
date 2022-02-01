4a509bff8e78b9c6f91f2f182d542a88fb27f514
dankarger <karger.dan@gmail.com>
2022-02-02 01:19:16 +0200

    add mui and test components 2

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import myApi from '../api/Api';
 // import SimpleAccordion from "../components/Accordion/Accordion";
 import ListDividers from "../components/Divider/Divider";
 import StandardImageList from "../components/ImageList/ImageList";
-
+import ActionAreaCard from "../components/Card/Card";
 const HomePage = () => {
     const [imageIds, setImageId] = useState([]);
     const [imageIds2, setImageId2] = useState();
@@ -49,13 +49,15 @@ const HomePage = () => {
             <div>
                 <h1 className='title'>Home3</h1>
                 {imageIds && imageIds.map((imageId, index) => (
-                    <Image
+                    <ActionAreaCard image={ <Image
                         key={index}
                         cloudName="meme3"
                         publicId={imageId}
                         width="300"
                         crop='scale'
-                    />
+                    />}/>
+
+
                 ))}
             </div>
         </div>
