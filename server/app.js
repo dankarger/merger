7d3d3eac2c2e824130d8089f87116e59faf0b9af
dankarger <karger.dan@gmail.com>
2022-02-01 15:07:29 +0200

    add cloudinary.js build project structure

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
 const cors = require('cors');
 const  app = express();
+const PORT = process.env.PORT || 8080 ;
+
+
+
+let publicPath = path.join(__dirname, 'client/build')
+if(PORT===8080){
+    publicPath = path.join(__dirname, 'client/public')
+}
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static());
+app.use(express.static(__dirname + publicPath));
 
 
 
@@ -16,7 +24,6 @@ app.use('*', (req,res)=> {
     res.send("this routes does not exist")
 });
 
-const PORT = process.env.PORT || 8080 ;
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
