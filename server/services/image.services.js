f119b6b0d982167807df296ad433e0b6c6ad19d2
dankarger <karger.dan@gmail.com>
2022-02-06 02:15:10 +0200

    add Skeleton components in Workspace page2

--- server/services/image.services.js
+++ server/services/image.services.js
@@ -54,7 +54,7 @@ const uploadImage = async (req,res)=> {
                              font_size: 80,
                              text:overlay},
                              // color: color,quality: "auto",width:"1.0",height:"1.0",gravity:"center", x:( x-300) / windowSize[0] ,y:(y-300)/ windowSize[1]},
-                               color: color,quality: "auto",width:"1.0",height:"1.0",gravity:"center", x:imageSize[0]/x, y:y/imageSize[1]/y },
+                               color: color,quality: "auto",width:"1.0",height:"1.0",gravity:"center", x:x-600, y:y-400 },
 
     ]},function(error, result) { console.log(result, error) })
         console.log('d', upLoadResponse2)
