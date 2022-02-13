161fde0d7d50ad6a008bd8139297a84332e37de7
dankarger <karger.dan@gmail.com>
2022-02-13 02:14:16 +0200

    fix svg in homepage

--- client/src/styles/HomePageSVG.styled.js
+++ client/src/styles/HomePageSVG.styled.js
@@ -3,14 +3,14 @@ import {motion} from 'framer-motion'
 
 export const HomePageSVGStyled = styled(motion.div) `
  //  
- width:100%;
-  height:100%;
+ width:100vw;
+  height:100vh;
   //min-width:1500px;
   //margin:0 auto;
   position: fixed;
-  //transform: translate(-50%,-50%);
-  //left: 50%;
-  //top: 50%;
+  transform: translate(-50%,-50%);
+  left: 43%;
+  top: 40%;
   opacity:  1;
   color:#333333;
   z-index: -10;
