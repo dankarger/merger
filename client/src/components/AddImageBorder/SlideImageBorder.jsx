8ba6fc4ea7e874f30b8fe691bd46f85a0905f280
dankarger <karger.dan@gmail.com>
2022-09-04 21:57:38 +0300

    changing Slider style in upload Image Tab

--- client/src/components/AddImageBorder/SlideImageBorder.jsx
+++ client/src/components/AddImageBorder/SlideImageBorder.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import {SlideImageBorderStyled} from "../../styles/SlideImageBorder.styled";
+import PedroSlider from "../interface/PedroSlider";
 
 export default function SlideImageBorder({
                                              imageBorderWidth,
@@ -23,14 +24,20 @@ export default function SlideImageBorder({
             <SlideImageBorderStyled>
                 <hr/>
                 <span>Border Width</span>
-                <Slider value={imageBorderWidth} aria-label="default" valueLabelDisplay="auto" onChange={handleChange}/>
-                <br/>
-                <hr/>
+                {/*<Slider color="secondary" value={imageBorderWidth} aria-label="default" valueLabelDisplay="auto" onChange={handleChange}/>*/}
+                <PedroSlider
+                    value={imageBorderWidth} aria-label="default"
+                    valueLabelDisplay="auto" onChange={handleChange}
+                />
                 <span>Border Radius</span>
-                <Slider value={imageBorderRadius} aria-label="default" valueLabelDisplay="auto"
-                        onChange={handleChangeRadius}/>
-                <hr/>
+                {/*<Slider value={imageBorderRadius} aria-label="default" valueLabelDisplay="auto"*/}
+                {/*        onChange={handleChangeRadius}/>*/}
+                <PedroSlider
+                    value={imageBorderRadius} aria-label="default" valueLabelDisplay="auto"
+                    onChange={handleChangeRadius}
+                />
             </SlideImageBorderStyled>
+
         </Box>
     );
 }
