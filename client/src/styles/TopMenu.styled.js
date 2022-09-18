37a687351ec6cf6fdbaa0d10e69b99ee06454871
dankarger <karger.dan@gmail.com>
2022-09-18 20:02:18 +0300

    fix footer and navbar width

--- client/src/styles/TopMenu.styled.js
+++ client/src/styles/TopMenu.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {motion} from 'framer-motion'
 
 export const TopMenuStyled = styled(motion.div) `
-  //width: 100%;
+  width: 100%;
   font-size: calc(1rem + 2vmin);
   letter-spacing: 3px;
   font-weight: 800;
