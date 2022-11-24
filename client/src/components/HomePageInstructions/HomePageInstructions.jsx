6eff5ba30d4ca5842dd6ddeaab40aa340761bd93
dankarger <karger.dan@gmail.com>
2022-11-24 11:42:38 +0200

    add video to HomePage Instructions

--- client/src/components/HomePageInstructions/HomePageInstructions.jsx
+++ client/src/components/HomePageInstructions/HomePageInstructions.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {HomePageInstructionsStyled} from "../../styles/HomePageInstructions.styled";
 
 const HomePageInstructions =() => {
+    const [ isVisible, setIsVisible] = useState(false)
+    const containerRef = useRef(null)
+    const videoRef = useRef(null)
 
+    const callBackFunction = (entries) => {
+        const [ entry ] = entries
+        setIsVisible(entry.isIntersecting)
+
+    }
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold:1.0
+    }
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver(callBackFunction, options)
+        if (containerRef.current) observer.observe(containerRef.current)
+
+        return ()=> {
+            if(containerRef.current) observer.unobserve(containerRef.current)
+        }
+    },[containerRef, options])
+    useEffect(()=>{
+        if(isVisible) videoRef.current.play()
+        else {
+            videoRef.current.pause()
+        }
+    },[isVisible])
     return (
         <HomePageInstructionsStyled>
             <div className="instructions">
-                <h2>✩ Add an image</h2>
-                <h2>✩ Add 2 Text layers</h2>
-                <h2>✩ Add background color</h2>
-                <h2>✩ Export to different formats and download</h2>
-                <h2>✩ Have a Gallery of your images</h2>
-                <h2>✩ Have fun :)</h2>
+
+
+                <div className="details">
+                    <h2>✩ Add an image</h2>
+                    <h2>✩ Add 2 Text layers</h2>
+                    <h2>✩ Add background color</h2>
+                    <h2>✩ Export to different formats and download</h2>
+                    <h2>✩ Have a Gallery of your images</h2>
+                    <h2>✩ Have fun :)</h2>
+                </div>
+                <div className="video-div" ref={containerRef}>
+                    <video  width="100%" height="auto"  controls={"controls"} ref={videoRef} muted loop={true}>
+                        <source src="https://res.cloudinary.com/meme3/video/upload/v1669280049/videos-merger/merger1-music3_tvjg3e.mp4" type="video/mp4" />
+
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
             </div>
 
         </HomePageInstructionsStyled>
--- client/src/styles/HomePageInstructions.styled.js
+++ client/src/styles/HomePageInstructions.styled.js
@@ -4,20 +4,39 @@ import {motion} from 'framer-motion'
 export const HomePageInstructionsStyled = styled(motion.div) `
   //width: 100%;
   //max-width: 100%;
-  padding: 10%;
-  height: 50vh;
+  //padding: 10%;
+  height: 100vh;
   background: rgba(23, 87, 66, 0.79);
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   //flex-direction: column;
   .instructions {
     color: whitesmoke;
     display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+    //flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    //gap: 3rem;
+  }
+  .details {
+    width:45%;
+    display: flex;
     justify-content: center;
-    gap: 2rem;
+    align-items: flex-start;
+    gap: 1rem;
+    flex-direction: column;
+    padding-left: 1rem;
+  }
+  .video-div {
+    padding:50px;
+    width: 45%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  video {
+    object-fit: cover;
   }
 
   @media (max-width: 900px) {
